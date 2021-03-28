226733df4b608682a8eca347ed3de91a5965409f
salamidrus <idrussalam95gmail.com>
2021-03-28 20:35:31 +0700

    Add Session One Notebooks

---
+++ Session_1/math_module.py
@@ -0,0 +1,11 @@
+def addition(*args):
+    sum=0
+    for arg in args:
+        sum+=arg
+    return sum
+
+def multiplication(*args):
+    mul=1
+    for arg in args:
+        mul*=arg
+    return mul
